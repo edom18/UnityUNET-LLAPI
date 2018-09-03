0956d3ff80636be4d335db35a9c61aa401659604
k-hiruma <k-hiruma@colopl.co.jp>
2018-09-03 12:44:11 +0900

    テストコードのベースを追加

---
+++ Assets/UNET-LLAPI/Scripts/Editor/NetworkBufferTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+using Assert = UnityEngine.Assertions.Assert;
+
+public class NetworkBufferTest
+{
+    private byte[] ByteCopy(NetworkBuffer buffer)
+    {
+        byte[] data = buffer.GetByte();
+        byte[] copy = new byte[data.Length];
+        Array.Copy(data, copy, data.Length);
+        return copy;
+    }
+
+    [Test]
+    public void ReadByteTest()
+    {
+        NetworkBuffer buffer = new NetworkBuffer();
+
+        byte a = 1;
+        buffer.Write(a);
+
+        byte[] copy = ByteCopy(buffer);
+        buffer.SetBytes(copy);
+
+        byte b = buffer.ReadByte();
+
+        Assert.AreEqual(a, b);
+    }
+}
